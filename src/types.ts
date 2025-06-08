d6a6038010c6f189517550d4956de22b367b1fd8
michioxd <michioxd@users.noreply.github.com>
2025-06-08 15:59:13 +0000

    deploy: b79f5ecd4bb0e1db6cce5cf9d6fe568901274cc3

--- src/types.ts
+++
@@ -1,6 +0,0 @@
-export const ShapeType = {
-    Circle: 0,
-    Polygon: 1,
-    NStar: 2,
-    Heart: 3,
-} as const;
--- src/utils.ts
+++
@@ -1,8 +0,0 @@
-export const formatTime = (ms: number) => {
-    if (ms <= 100) {
-        return `${ms}ms`;
-    } else {
-        const seconds = ms / 1000;
-        return `${seconds.toFixed(4)}s`;
-    }
-};
